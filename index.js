0055f401a3854fb1f17c8cd2c1c1caff35dce652
Mandibol <92530271+Mandibol@users.noreply.github.com>
2021-12-10 05:53:12 +0100

    Almost done

--- index.js
+++ index.js
@@ -16,36 +16,71 @@ app.get("/", (req, res) => {
 //Start Server
 server.listen(3000);
 console.log("kör servern på localhost:3000");
+
 let usersOnline = [];
+let chatLog = [];
+
+//Function to escape bad html
+function escapeHTML(obj) {
+    const tempObj = obj;
+    for (let x in obj) {
+        tempObj[x] = (obj[x]).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+    };
+    return tempObj;
+}
 
 
 //User connects
 io.on("connection", (socket) => {
+    //Set userName
     socket.userName = socket.handshake.auth.userName;
-    usersOnline.push(socket.userName); 
+
+    //Create userObj
+    userObj = {};
+    userObj.userName = socket.userName;
+    userObj.id = socket.id;
+
+    //Store userObj in Array
+    usersOnline.push(userObj);
+
+    //Broadcast that a user has joined the chat 
     socket.broadcast.emit('user_action', socket.userName + ' joined');
     console.log(socket.userName + " Connected")
-    //Send usersonline
-    console.log(usersOnline)
-    io.emit("users_online",usersOnline);
+
+    //Send Chatlog
+    socket.emit("chat_log", chatLog);
+
+    //Send Welcome Message from server
+    socket.emit("user_action", "Välkommen " + socket.userName + "!");
 
     //User Disconnects
     socket.on("disconnect", () => {
-        usersOnline = usersOnline.filter(item => item !== socket.userName);
+        usersOnline = usersOnline.filter(item => item.id !== socket.id);
         console.log(socket.userName + " Disconnected")
-        console.log(usersOnline)
-        //Send usersonline
-        io.emit("users_online",usersOnline);
         socket.broadcast.emit('user_action', socket.userName + ' disconnected');
     });
 
     //Manage messages
     socket.on("message", (data) => {
-        const msgObj = {};
-        msgObj.sender = socket.userName;
-        msgObj.time = new Date().toISOString();
-        msgObj.message = data;
-        socket.broadcast.emit("message", msgObj);
+        data = escapeHTML(data);
+        chatLog.push(data);
+        socket.broadcast.emit("message", data);
+    });
+
+    socket.on("private_message", (data) => {
+        socket.to(data.id).emit("private_message", data);
+    });
+
+    //Send userOnline
+    socket.on("users_online", () => {
+        let users = usersOnline.filter(item => item.id !== socket.id);
+        socket.emit("users_online",users);
+    });
+
+    //Send user is writing
+    socket.on("user_writing", (data) => {
+        let msg = (data) ? socket.userName + " skriver...":"";
+        socket.broadcast.emit("user_action", msg);
     });
 });
 
--- public/javascript/clientscript.js
+++ public/javascript/clientscript.js
@@ -6,46 +6,121 @@ function parseMessage(data){
     return html;     
 };
 
-window.onload = () => {   
-    const output = document.getElementById("outputInner");
-    const input = document.getElementById("inputInner");
-    const inputForm = document.getElementById("inputOuter");
-    const usersDisplay = document.getElementById("usersInner");
-    const userName = prompt("Välj Ett användarnamn");
-    socket.auth = { userName };
-    socket.connect();
+window.onload = () => {  
+    const userNameInputForm = document.getElementById("userNameInputForm");
+    const userNameInput = document.getElementById("userNameInput");
+    const serverMsgTD = document.getElementById("serverMsgTD");
+    const chat = document.getElementById("chat");
+    const output = document.getElementById("chatOutput");
+    const input = document.getElementById("chatInput");
+    const inputForm = document.getElementById("chatInputForm");
+    const usersDisplay = document.getElementById("usersDisplay");
+    const selectUser = document.getElementById("selectUser");
+    let userList = [];
     
+    //Scrolla
+    let prevScrollTop = 0;
+    function autoScroll(){
+        if (prevScrollTop <= output.scrollTop){
+            output.scrollTop = output.scrollHeight;
+            prevScrollTop = output.scrollTop;
+        }
+    }
+
+    //Login
+    let userName = false;
+    userNameInputForm.addEventListener("submit", (evt) =>{
+        evt.preventDefault();
+        userName = userNameInput.value;
+        socket.auth = { userName };
+        socket.connect();
+        //Show Chat and hide login
+        userNameInputForm.reset();
+        userNameInputForm.style.display = "none";
+        chat.style.display = "table";
+    });
+    
+    //User is writing
+    input.addEventListener("input", () => {
+        if (input.value !== "") {
+            socket.emit("user_writing", true);
+        }
+        else {
+            socket.emit("user_writing", false); 
+        }
+    });
+
     //Send Message
     inputForm.addEventListener("submit", (evt) =>{
         evt.preventDefault();
         let message = input.value;
-        socket.emit("message", message);
         input.value = "";
-
         const msgObj = {};
         msgObj.sender = userName;
         msgObj.time = new Date().toISOString()
         msgObj.message = message;
+        //send message to all
+        if (selectUser.value === "none"){
+            socket.emit("message", msgObj);
+            msgObj.sender = "Du skrev:";
+        }
+        //send private message
+        else{
+            msgObj.id = selectUser.value;
+            msgObj.sender = `${msgObj.sender} viskar:`
+            socket.emit("private_message", msgObj)
+            let user = userList.find(obj => obj.id === selectUser.value);
+            msgObj.sender = `Du viskar till: ${user.userName}`
+        }
         output.innerHTML += parseMessage(msgObj);
+        autoScroll();
+        socket.emit("user_writing", false);
     });
 
     //Recieve Message
     socket.on("message", (data) => {
-        output.innerHTML += parseMessage(data); 
+        output.innerHTML += parseMessage(data);
+        autoScroll();
     });
 
-    //A user connected
+    //Recieve private Chat
+    socket.on("private_message", (data) => {
+        output.innerHTML += parseMessage(data);
+        autoScroll();
+    });
+
+    //A user connected/disconnected
     socket.on("user_action", (data) => {
-        output.innerHTML += '<h5>' + data + '</h5>';
+        serverMsgTD.innerHTML = '<h5>' + data + '</h5>';
+        socket.emit("users_online", false);
+    });
+
+    //get Chatlog
+    socket.on("chat_log", (data) => {
+        data.forEach(obj => {
+            output.innerHTML += parseMessage(obj);
+            autoScroll();
+        });
     });
 
     //get list of all users
     socket.on("users_online", (data) => {
+
+        userList = data;
         let html = "";
+        //Show who is Online
         data.forEach(element => {
-            html += '<p>' + element + '</p>';
+            html += `<p>${element.userName}</p>`;
         });
         usersDisplay.innerHTML = html;
+
+        //Create Select list for channels
+        html = `<option value="none">Alla</option>`
+        data.forEach(user => {
+            console.log(user)
+            html += `<option value="${user.id}">${user.userName}</option>`;
+        });
+        selectUser.innerHTML = html;
     });
 
     socket.onAny((event, ...args) => {
